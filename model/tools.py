cd00de3d24046ca2cdc72e246a25e5a1e0306858
wolfhardfehre <ulla.umme@googlemail.com>
2015-03-25 19:29:01 +0100

    test comparison between deCasteljau and bernstein bezier splines

--- model/tools.py
+++ model/tools.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import numpy as np
+import timeit
 
 
 class Point():
@@ -52,7 +53,7 @@ def bezier(t0, points, n):
 
 
 def deCasteljau(t0, points, n):
-    ply = np.array(points)
+    ply = points[:]
     for j in range(n):
         for i in range(n-j):
             ply[i] = (1 - t0) * ply[i] + t0 * ply[i + 1]
@@ -82,16 +83,39 @@ def createSublines(t_max, pts_x, pts_y):
         sub_y.append(pts_y)
     return sub_x, sub_y
 
+# PARAMS ##################################
+T_MAX = 0.6
+'''
+POINTS = [Point(0.0, 0.0), Point(25.0, 50.0),
+          Point(50.0, 0.0), Point(-50.0, -25.0)]
+'''
+POINTS = [Point(0.0, 0.0), Point(1.0, 1.0),
+          Point(2.0, 2.0), Point(3.0, 3.0),
+          Point(4.0, 4.0), Point(5.0, 5.0),
+          Point(6.0, 6.0), Point(7.0, 7.0),
+          Point(8.0, 8.0), Point(9.0, 9.0),
+          Point(10.0, 10.0), Point(11.0, 11.0),
+          Point(12.0, 10.0), Point(13.0, 9.0),
+          Point(14.0, 8.0), Point(15.0, 7.0),
+          Point(16.0, 6.0), Point(17.0, 5.0),
+          Point(18.0, 4.0), Point(19.0, 3.0),
+          Point(20.0, 2.0), Point(21.0, 1.0),
+          Point(22.0, 0.0), Point(23.0, -1.0),
+          Point(24.0, -2.0), Point(25.0, -3.0),
+          Point(26.0, -4.0), Point(27.0, -5.0),
+          Point(28.0, -6.0), Point(29.0, -7.0),
+          Point(30.0, -8.0), Point(31.0, -9.0)]
+
+N = len(POINTS) - 1
+###########################################
 
 if __name__ == '__main__':
-    # PARAMS ##################################
-    T_MAX = 0.6
-    POINTS = [Point(0.0, 0.0), Point(25.0, 50.0),
-              Point(50.0, 0.0), Point(-50.0, -25.0)]
-    N = len(POINTS) - 1
-    ###########################################
-    xs, ys = createBezierSpline(T_MAX, POINTS)
-    print xs[-1], ys[-1]
 
-    print bezier(T_MAX, POINTS, N)
-    print deCasteljau(T_MAX, POINTS, N)
+    print "x = %6.3f, y = %6.3f" % tuple(bezier(T_MAX, POINTS, N))
+    print "x = %6.3f, y = %6.3f" % tuple(deCasteljau(T_MAX, POINTS, N))
+    print "deCasteljau: %7.4f s" % timeit.timeit('deCasteljau(T_MAX, POINTS, N)',
+                                                 setup='from tools import deCasteljau, T_MAX, POINTS, N',
+                                                 number=1000)
+    print "     bezier: %7.4f s" % timeit.timeit('bezier(T_MAX, POINTS, N)',
+                                                 setup='from tools import bezier, T_MAX, POINTS, N',
+                                                 number=1000)
